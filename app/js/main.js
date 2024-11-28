76965b4b11e33bf20a6b5a9b0306778195e71717
lebowski505 <dmitriysalienko@gmail.com>
2024-11-28 15:11:04 +0200

    added shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,42 @@
-$(function(){
-  
+$(function () {
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list');
+  });
+  $('.button-grid').on('click', function(){
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.select-style').styler();
+
+  var slider = document.querySelector('.filter-price__slider'); 
+  var tooltip = document.getElementById('tooltip');
+
+  if (slider) {
+    noUiSlider.create(slider, {
+      start: [200, 800], 
+      connect: true,   
+      range: {         
+        min: 0,
+        max: 999
+      },
+      tooltips: false,  
+    });
+
+    slider.noUiSlider.on('update', function(values) {
+      var minValue = Math.round(values[0]);
+      var maxValue = Math.round(values[1]);
+      tooltip.innerHTML = "Price: $" + minValue + " - $" + maxValue;
+    });
+
+  } else {
+    console.error('Slider error!');
+  }
 
   $('.top-slider__inner').slick({
     dots: true,
@@ -8,7 +45,7 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 2000
   });
-  
+
   $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
@@ -22,7 +59,7 @@ $(function(){
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -31,31 +68,31 @@ $(function(){
       seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
     const clock = document.querySelector('.promo__clock');
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
-  
+
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
 
