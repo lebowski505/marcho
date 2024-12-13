5f52e204d1e21c5e6856dd6a33eb14739d40d049
lebowski505 <dmitriysalienko@gmail.com>
2024-12-13 22:20:44 +0200

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(function () {
       $('.menu__list').toggleClass('menu__list--active');
     });
 
+    $('.shop__filter-btn').on('click', function(){
+        $('.shop__filters').slideToggle('shop__filters--active');
+      });
+
   let map;
 
   async function initMap() {
@@ -272,7 +276,15 @@ $(function () {
     asNavFor: '.product-slide__thumb',
     draggable: false,
     arrows: false,
-    fade: true
+    fade: true,
+    responsive: [
+        {
+            breakpoint: 1051,
+            settings: {
+                draggable: true,
+            }
+        },
+    ]
   });
 
   $('.shop-content__filter-btn').on('click', function () {
@@ -282,9 +294,11 @@ $(function () {
 
   $('.button-list').on('click', function () {
     $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__nogrid');
   });
   $('.button-grid').on('click', function () {
     $('.product-item').removeClass('product-item--list');
+    $('.shop-content__inner').removeClass('shop-content__nogrid');
   });
 
   $('.select-style, .product-one__num').styler();
